49a181ee7c24e5d13705946cc337fdeb4cc56227
wgoni <raphael.gonin@hes-so.ch>
2025-04-28 08:41:26 +0200

    Cleaned up and commented

--- RSA.py
+++ RSA.py
@@ -10,14 +10,18 @@ class RSA:
         self.public_key = self.public_key_gen()
         self.private_key = self.private_key_gen()
 """
+
+    # Non utilisé dans le programme
     def phi(self):
         return (self.p - 1) * (self.q - 1)
 
+    # Non utilisé dans le programme
     def public_key_gen(self):
         n = self.p * self.q
         public_key = [self.e, n]
         return public_key
 
+    # Non utilisé dans le programme
     def private_key_gen(self):
         n = self.p * self.q
         d = PS.PrimeStuff.modInverse(self.e, self.phi())
@@ -28,6 +32,7 @@ class RSA:
         encrypted = msg ** public_key[0] % public_key[1]
         return encrypted
 
+    # Non utilisé dans le programme (decrypt pas implémenté)
     def RSADecrypt(self, encrypted, private_key):
         decrypted = encrypted ** private_key[0] % private_key[1]
         return decrypted
